2e41a9759246c55fb7eb42ddb8529114558f0d71
Wellington Kalebi <wellington.kalebi@outlook.com>
2024-04-23 21:53:59 -0300

    Criado o Sidebar, protegendo rotas com AuthHOC, tambem Outlet. Criando parte do input

---
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -0,0 +1,10 @@
+import { Input } from "../../../components/common/Input";
+import { Panel } from "../../../components/layout/Panel";
+
+export function Shelter(){
+    return (
+    <Panel>
+        <Input label="Nome" />
+    </Panel>
+    )
+}
